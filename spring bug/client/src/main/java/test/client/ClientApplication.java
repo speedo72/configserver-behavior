735191003f998e2572d1c46876d20e7c51aa9d10
Marc Pinto <marcpinto99@gmail.com>
2022-04-11 18:55:56 -0400

    9

--- spring bug/client/src/main/java/test/client/ClientApplication.java
+++ spring bug/client/src/main/java/test/client/ClientApplication.java
@@ -2,6 +2,8 @@ package test.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.client.ConfigClientProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientApplication {
@@ -10,4 +12,10 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
+
+	@Bean
+	public String test(ConfigClientProperties props) {
+		final String a = "test";
+		return "test";
+	}
 }
